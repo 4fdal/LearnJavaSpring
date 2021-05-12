90f3b450157180e7af25232fa62a71c79ed6d19c
my4fdal <my4fdal@gmail.com>
2021-05-12 21:26:54 +0700

    fresh insatll

---
+++ src/main/java/com/learn/restfull/LearnRestfullApplication.java
@@ -0,0 +1,13 @@
+package com.learn.restfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnRestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnRestfullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/restfull/learnrestfull/LearnRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.restfull.learnrestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
